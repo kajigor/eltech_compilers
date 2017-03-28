c287ac109d2b0d05988fe4e41c61f11d0a78b02c
dsv <dsv@sofitlabs.ru>
2017-03-28 19:54:28 +0300

    update from master

--- SemyonDanilov/installDeps.sh
+++ SemyonDanilov/installDeps.sh
@@ -1,5 +1,6 @@
 opam pin add GT https://github.com/Kakadu/GT.git -n -y
+opam pin add ostap https://github.com/dboulytchev/ostap.git -n -y
 opam install camlp5 -
-opam install GT ocamlfind -y
+opam install GT ostap ocamlfind -y
 eval `opam config env`
 sudo apt-get install gcc-multilib -y
