f4c5a425de7a78cd1b9f1ece8428514c2a49fcf5
Dron642 <chulanov1994@mail.ru>
2017-04-18 23:56:45 +0300

    13 Expr and Stack Machine

---
+++ ChulanovAndrey/src/Language.ml
@@ -0,0 +1,45 @@
+open GT
+
+(* AST for expressions *)
+module Expr =
+  struct
+
+    @type t =
+    | Var   	of string
+    | Const 	of int
+    | Add   	of t * t
+    | Mul   	of t * t 
+	| Sub   	of t * t
+    | Div   	of t * t
+    | Mod   	of t * t
+    | Or    	of t * t
+    | And       of t * t
+    | Equal     of t * t
+    | NotEqual  of t * t
+    | Less      of t * t
+    | More      of t * t
+    | LessEqual of t * t
+    | MoreEqual of t * t
+
+  end
+
+(* AST statements/commands *)
+module Stmt =
+  struct
+
+    @type t =
+    | Skip
+    | Assign of string * Expr.t
+    | Read   of string
+    | Write  of Expr.t
+    | Seq    of t * t with show
+
+  end
+
+module Program =
+  struct
+
+    type t = Stmt.t
+
+  end
+
