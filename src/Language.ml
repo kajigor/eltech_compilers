2ed0d3ad153638df042ddaccb821a65166e04397
dsv <dsv@sofitlabs.ru>
2017-05-09 10:42:16 +0300

    tokenize new operators

--- src/Language.ml
+++ src/Language.ml
@@ -61,6 +61,9 @@ module Stmt =
     | Read   of string
     | Write  of Expr.t
     | Seq    of t * t
+    | If     of Expr.t * t * t
+    | While  of Expr.t * t
+    | Repeat of t * Expr.t
 
     let expr = Expr.parse
 
@@ -68,8 +71,22 @@ module Stmt =
       simp: x:IDENT ":=" e:expr  {Assign (x, e)}
       | %"read"  "(" x:IDENT ")" {Read x}
       | %"write" "(" e:expr  ")" {Write e}
-      | %"skip"                  {Skip};
-      
+      | %"skip"                  {Skip}
+      | %"if"        e:!(Expr.parse)
+        %"then"      s1:!(parse)
+        %"else"      s2:!(parse)
+        %"fi"                            {If (e, s1, s2)}
+      | %"if"        e:!(Expr.parse)
+        %"then"      s1:!(parse)
+        %"fi"                            {If (e, s1, Skip)}
+      | %"while"     e:!(Expr.parse)
+        %"do"        s:!(parse)
+        %"od"                            {While (e, s)}
+      | %"repeat"    s:!(parse)
+        %"until"     e:!(Expr.parse)     {Seq (s, While (Binop ("==", e, Const 0), s))}
+      | %"for"       i:!(parse) "," n:!(Expr.parse) "," b:!(parse)
+        %"do"        a:!(parse)
+        %"od"                            {Seq (i, (While (n, Seq (a, b))))};
       parse: s:simp ";" d:parse {Seq (s,d)} | simp 
     )
 
