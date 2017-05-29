ffc596cac1d5b17acc764c9507f24bf7c20d1df0
Shirru <annafreia@mail.ru>
2017-05-29 17:30:12 +0300

    BinOp and constructs

--- src/Interpret.ml
+++ src/Interpret.ml
@@ -6,15 +6,31 @@ module Expr =
 
     open Expr
 
+
+   let match_operation op = match op with 
+	| "+"  ->  fun x y -> x + y
+	| "-"  ->  fun x y -> x - y
+	| "*"  ->  fun x y -> x * y			
+	| "/"  ->  fun x y -> x / y	
+	| "%"  ->  fun x y -> x mod y
+	| "==" ->  fun x y -> if x = y  then 1 else 0
+	| "!=" ->  fun x y -> if x != y then 1 else 0
+	| ">"  ->  fun x y -> if x > y  then 1 else 0
+	| "<"  ->  fun x y -> if x < y  then 1 else 0
+	| ">=" ->  fun x y -> if x >= y then 1 else 0
+	| "<=" ->  fun x y -> if x <= y then 1 else 0
+	| "&&" ->  fun x y -> if (x = 0 || y = 0) then 0 else 1
+	| "!!" ->  fun x y -> if (x = 0 && y = 0) then 0 else 1
+
     let rec eval expr st = 
       let eval' e = eval e st in
       match expr with
-      | Var   x     -> st x
-      | Const z     -> z
-      | Add  (x, y) -> eval' x + eval' y
-      | Mul  (x, y) -> eval' x * eval' y
+      | Var   x             -> st x
+      | Const z             -> z
+      | BinOp  (op, x, y)   -> (match_operation op)(eval' x)(eval' y)
 
   end
+
 
 (* Interpreter for statements *)
 module Stmt =
@@ -22,18 +38,39 @@ module Stmt =
 
     open Stmt
 
-    (* State update primitive *) 
-    let update st x v = fun y -> if y = x then v else st y 
-      
+    (* State update primitive *)
+    let update st x v = fun y -> if y = x then v else st y
+
     let rec eval stmt ((st, input, output) as conf) =
       match stmt with
-      | Skip          -> conf
-      | Assign (x, e) -> (update st x (Expr.eval e st), input, output)
-      | Read    x     -> 
-	  let z :: input' = input in 
+      | Skip                  -> conf
+      | Assign (x, e)         -> (update st x (Expr.eval e st), input, output)
+      | Read    x             ->
+          let z :: input' = input in (* :: берет верхний элемента списка входных данных *)
           (update st x z, input', output)
-      | Write   e     -> (st, input, output @ [Expr.eval e st])
-      | Seq (s1, s2)  -> eval s1 conf |> eval s2 
+      | Write   e             -> (st, input, output @ [Expr.eval e st])
+      | Seq (s1, s2)          -> eval s1 conf |> eval s2 (*вычисляется s1 и conf передается в s2 *)
+      | If  (exp, seq1, seq2) -> 
+          if (Expr.eval exp st) != 0 
+          then eval seq1 conf 
+          else eval seq2 conf
+      | While (exp, seq)      -> 
+          (*recursive loop function*)
+          let rec loop exp' seq' ((st', _, _) as conf') = 
+            if (Expr.eval exp' st') != 0 
+            then loop exp' seq' (eval seq' conf') 
+            else conf' 
+          in
+          loop exp seq conf
+      | Repeat (seq, exp)      ->
+          (*recursive loop function*)
+          let rec loop exp' seq' conf' = 
+            let (newst, _, _) as newconf = eval seq' conf' in
+            if (Expr.eval exp' newst) == 0
+            then loop exp' seq' newconf
+            else newconf
+          in
+          loop exp seq conf
 
   end
 
@@ -42,7 +79,7 @@ module Program =
 
     let eval p input = 
       let (_, _, output) = 
-	Stmt.eval p ((fun _ -> failwith "undefined variable"), input, []) 
+        Stmt.eval p ((fun _ -> failwith "undefined variable"), input, []) 
       in
       output
 
--- src/X86.ml
+++ src/X86.ml
@@ -3,98 +3,168 @@ open Instr
 
 type opnd = R of int | S of int | L of int | M of string
 
-let regs  = [|"%eax"; "%ebx"; "%ecx"; "%esi"; "%edi"; "%edx"; "%esp"; "%ebp"|]
-let nregs = Array.length regs - 3
+let regs  = [|"%ebx"; "%ecx"; "%esi"; "%edi"; "%eax"; "%edx"; "%esp"; "%ebp"|]
 
-let [|eax; ebx; ecx; esi; edi; edx; esp; ebp|] = Array.mapi (fun i _ -> R i) regs
+let [|al; dl|] = [|"%al"; "%dl"|]
+
+let nregs = Array.length regs - 4
+
+let [|ebx; ecx; esi; edi; eax; edx; esp; ebp|] = Array.mapi (fun i _ -> R i) regs
 
 type instr =
-| Add  of opnd * opnd
-| Mul  of opnd * opnd
-| Mov  of opnd * opnd
-| Push of opnd
-| Pop  of opnd
-| Call of string
-| Ret
+  | Add  of opnd * opnd
+  | Sub  of opnd * opnd
+  | Mul  of opnd * opnd
+  | Div  of opnd
 
-let to_string buf code =      
+  | Or   of opnd * opnd
+  | And  of opnd * opnd
+
+  | Mov  of opnd * opnd
+  | Push of opnd
+  | Pop  of opnd
+  | Call of string
+  | Ret
+  
+  | Cmp     of opnd * opnd
+  | Set     of string * string
+  | Lbl     of int
+  | Jz      of int
+  | Jnz     of int
+  | Jmp     of int
+  | Test    of opnd * opnd
+  | Cltd
+
+let to_string buf code =
   let instr =
     let opnd = function
       | R i -> regs.(i)
       | S i -> Printf.sprintf "%d(%%ebp)" (-i * 4)
       | L i -> Printf.sprintf "$%d" i
-      | M s -> s  
+      | M s -> s
     in
     function
-      | Add (x, y) -> Printf.sprintf "addl\t%s,%s"  (opnd x) (opnd y)
-      | Mul (x, y) -> Printf.sprintf "imull\t%s,%s" (opnd x) (opnd y)
-      | Mov (x, y) -> Printf.sprintf "movl\t%s,%s"  (opnd x) (opnd y)
-      | Push x     -> Printf.sprintf "pushl\t%s"    (opnd x)
-      | Pop  x     -> Printf.sprintf "popl\t%s"     (opnd x)
-      | Call x     -> Printf.sprintf "call\t%s"      x
-      | Ret        -> "ret" 
+      | Add (x, y)    -> Printf.sprintf "addl\t%s,%s"  (opnd x) (opnd y)
+      | Sub (x, y)    -> Printf.sprintf "subl\t%s,%s"  (opnd x) (opnd y)
+      | Mul (x, y)    -> Printf.sprintf "imull\t%s,%s" (opnd x) (opnd y)
+      | Div  x        -> Printf.sprintf "idiv\t%s"     (opnd x)
+      | Cltd          -> "cltd"
+      | Mov (x, y)    -> Printf.sprintf "movl\t%s,%s"  (opnd x) (opnd y)
+      | Push x        -> Printf.sprintf "pushl\t%s"    (opnd x)
+      | Pop  x        -> Printf.sprintf "popl\t%s"     (opnd x)
+      | Call x        -> Printf.sprintf "call\t%s"     x
+      | Ret           -> "ret"
+      | Or (x, y)     -> Printf.sprintf "orl\t%s,%s"   (opnd x) (opnd y)
+      | And (x, y)    -> Printf.sprintf "andl\t%s,%s"  (opnd x) (opnd y)
+      | Cmp (x, y)    -> Printf.sprintf "cmpl\t%s,%s"  (opnd x) (opnd y)
+      | Set (s, x)    -> Printf.sprintf "set%s\t%s"    s x
+      | Lbl x         -> Printf.sprintf "lbl%d:"       x
+      | Jz x          -> Printf.sprintf "jz\t\tlbl%d"  x
+      | Jnz x         -> Printf.sprintf "jnz\t\tlbl%d" x
+      | Jmp x         -> Printf.sprintf "jmp\t\tlbl%d" x
+      | Test (x, y)   -> Printf.sprintf "testl\t%s,%s" (opnd x) (opnd y)
   in
-  let out s = 
-    Buffer.add_string buf "\t"; 
-    Buffer.add_string buf s; 
-    Buffer.add_string buf "\n" 
+  let out s =
+    Buffer.add_string buf "\t";
+    Buffer.add_string buf s;
+    Buffer.add_string buf "\n"
   in
   List.iter (fun i -> out @@ instr i) code
-    
+
 module S = Set.Make (String)
-    
+
+
 class env =
   object (this)
     val locals = S.empty
     val depth  = 0
-	
+
     method allocate = function
       | []                          -> this, R 0
       | R i :: _ when i < nregs - 1 -> this, R (i+1)
       | S i :: _                    -> {< depth = max depth (i+1) >}, S (i+1)
       | _                           -> {< depth = max depth 1 >}, S 1 
-	    
+      
     method local     x = {< locals = S.add x locals >}
     method get_locals  = S.elements locals
     method get_depth   = depth
   end
     
+
 let rec sint env prg sstack =
   match prg with
   | []        -> env, [], []
   | i :: prg' ->
-      let env, code, sstack' = 
-	match i with
-	| PUSH n ->  
-            let env', s = env#allocate sstack in
-            env', [Mov (L n, s)], s :: sstack
-        | LD x ->
-            let env'     = env#local x in
-            let env'', s = env'#allocate sstack in
-            env'', [Mov (M x, s)], s :: sstack
-	| ST x ->
-            let env' = env#local x in
-            let s :: sstack' = sstack in
-            env', [Mov (s, M x)], sstack' 
-        | READ  ->
-            env, [Call "lread"], [eax]
-        | WRITE ->
-            env, [Push eax; Call "lwrite"; Pop edx], [] 
-        | _ ->
-            let x::(y::_ as sstack') = sstack in
-            (fun op ->
-              match x, y with
-              | S _, S _ -> env, [Mov (y, edx); op x edx; Mov (edx, y)], sstack'
-              | _        -> env, [op x y], sstack'   
-            )
-              (match i with 
-	      | MUL -> fun x y -> Mul (x, y)
-	      | ADD -> fun x y -> Add (x, y)
-              )
-      in
-      let env, code', sstack'' = sint env prg' sstack' in
-      env, code @ code', sstack''
-	
+    let env, code, sstack' =
+      match i with
+
+      | PUSH n ->
+        let env', s = env#allocate sstack in
+        env', [Mov (L n, s)], s :: sstack
+
+      | LD x ->
+        let env'     = env#local x in
+        let env'', s = env'#allocate sstack in
+        (match s with
+        | S _ ->  env'', [Mov (M x, edx); Mov (edx, s)], s :: sstack
+        | _   ->  env'', [Mov (M x, s)], s :: sstack)
+
+      | ST x ->
+        let env' = env#local x in
+        let s :: sstack' = sstack in
+        (match s with
+        | S _ ->  env', [Mov (s, edx); Mov (edx, M x)], sstack'
+        | _   ->  env', [Mov (s, M x)], sstack')
+
+      | READ  -> env, [Call "lread"], [eax]
+      | WRITE -> env, [Push ebx; Call "lwrite"; Pop edx], []
+      | LBL x -> env, [Lbl x], []
+      | JMP x -> env, [Jmp x], []
+
+      | CJMP (x,c) -> 
+        let s :: sstack' = sstack in
+        let jmpop = (match c with "z"  -> Jz x | "nz" -> Jnz x) in
+        (match s with
+        | S _ ->  env, [Mov (s, edx); Test (edx, edx); jmpop], []
+        | _   ->  env, [Test (s, s); jmpop], [])
+
+      | BINOP op ->
+        let x::(y::_ as sstack') = sstack in
+		let and_opr = [Mov (y, edx); And (y, edx);               
+		               Mov (L 0, edx); Set("ne", dl);               
+		               Mov (x, eax); And (x, eax);               
+		               Mov (L 0, eax); Set("ne", al);                    
+		               And (eax, edx); Mov (L 0, edx); Set("ne", dl); 
+		               Mov (edx, y)] in                             
+
+		let apply_operation x y = 
+			(match op with
+			| "+"  -> [Add (x, y)]
+			| "-"  -> [Sub (x, y)]
+			| "*"  -> [Mul (x, y)]
+			| "/"  -> [Mov (y, eax); Cltd; Div x; Mov (eax, y)]
+			| "%"  -> [Mov (y, eax); Cltd; Div x; Mov (edx, y)]
+			| "&&" -> and_opr
+			| "!!" -> [Mov (y, edx); Or(x, y); Mov (L 0, edx); Set("ne", dl); Mov (edx, y)]
+			| "==" -> [Cmp(x, y); Mov (L 0, edx); Set("e", dl); Mov (edx, y)]
+			| "!=" -> [Cmp(x, y); Mov (L 0, edx); Set("ne", dl); Mov (edx, y)]
+			| "<=" -> [Cmp(x, y); Mov (L 0, edx); Set("le", dl); Mov (edx, y)]
+			| ">=" -> [Cmp(x, y); Mov (L 0, edx); Set("ge", dl); Mov (edx, y)]
+			| ">"  -> [Cmp(x, y); Mov (L 0, edx); Set("g", dl); Mov (edx, y)]
+			| "<"  -> [Cmp(x, y); Mov (L 0, edx); Set("l", dl); Mov (edx, y)]
+              		) in
+
+			match x, y with
+			(*if both operands in stack*)
+			| S _, S _ -> env, [Mov (y, edx)] @ apply_operation x edx @[Mov (edx, y)], sstack'
+			(*if one or both operands in register*) 
+			| _ -> env, apply_operation x y, sstack' 
+         
+		    in
+		    let env, code', sstack'' = sint env prg' sstack' in
+		    env, code @ code', sstack''
+  
+
 let compile p = 
   let env, code, [] = sint (new env) (Compile.Program.compile p) [] in
   let buf   = Buffer.create 1024 in
@@ -113,8 +183,8 @@ let compile p =
   out "\tpopl\t%ebp\n";
   out "\tret\n";
   Buffer.contents buf
-    
-let build stmt name =
+
+  let build stmt name =
   let outf = open_out (Printf.sprintf "%s.s" name) in
   Printf.fprintf outf "%s" (compile stmt);
   close_out outf;
