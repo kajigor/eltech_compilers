06cd7c3591c786816dd9a2a9921429de086a53ec
smab90@Apofis_smab <smab90@mail.ru>
2017-05-31 03:58:23 +0300

    Smirnov 2381

--- src/Interpret.ml
+++ src/Interpret.ml
@@ -11,9 +11,19 @@ module Expr =
       match expr with
       | Var   x     -> st x
       | Const z     -> z
-      | Add  (x, y) -> eval' x + eval' y
-      | Mul  (x, y) -> eval' x * eval' y
-
+      | Add  (x, y) 	    -> eval' x + eval' y
+      | Sub  (x, y) 	    -> eval' x - eval' y
+      | Mul  (x, y) 	    -> eval' x * eval' y
+      | Div  (x, y) 	    -> eval' x / eval' y
+      | Divd  (x, y) 	    -> eval' x mod eval' y
+      | Or (x, y)   	    -> if ((eval' x) == 0) && ((eval' y) == 0) then 0 else 1
+      | And (x, y)  	    -> if ((eval' x) == 0) || ((eval' y) == 0) then 0 else 1
+      | Equal (x, y)        -> if (eval' x) == (eval' y) then 1 else 0
+      | NotEqual (x, y)     -> if (eval' x) != (eval' y) then 1 else 0
+      | Less (x, y)         -> if (eval' x) < (eval' y) then 1 else 0
+      | Greater (x, y)      -> if (eval' x) > (eval' y) then 1 else 0
+      | LessEqual (x, y)    -> if (eval' x) <= (eval' y) then 1 else 0
+      | GreaterEqual (x, y) -> if (eval' x) >= (eval' y) then 1 else 0
   end
 
 (* Interpreter for statements *)
@@ -34,6 +44,27 @@ module Stmt =
           (update st x z, input', output)
       | Write   e     -> (st, input, output @ [Expr.eval e st])
       | Seq (s1, s2)  -> eval s1 conf |> eval s2 
+      | If  (exp, pt1, pt2) -> 
+          if (Expr.eval exp st) != 0 
+          then eval pt1 conf 
+          else eval pt2 conf
+      | While (exp, pt)      -> 
+          (*recursive loop function*)
+          let rec loop exp' pt' ((st', _, _) as conf') = 
+            if (Expr.eval exp' st') != 0 
+            then loop exp' pt' (eval pt' conf') 
+            else conf' 
+          in
+          loop exp pt conf
+      | Until (pt, exp)      ->
+          (*recursive loop function*)
+          let rec loop exp' pt' conf' = 
+            let (newst, _, _) as newconf = eval pt' conf' in
+            if (Expr.eval exp' newst) == 0
+            then loop exp' pt' newconf
+            else newconf
+          in
+          loop exp pt conf
 
   end
 
--- src/X86.ml
+++ src/X86.ml
@@ -4,19 +4,35 @@ open Instr
 type opnd = R of int | S of int | L of int | M of string
 
 let regs  = [|"%eax"; "%ebx"; "%ecx"; "%esi"; "%edi"; "%edx"; "%esp"; "%ebp"|]
-let nregs = Array.length regs - 3
+let nregs = Array.length regs - 4
 
 let [|eax; ebx; ecx; esi; edi; edx; esp; ebp|] = Array.mapi (fun i _ -> R i) regs
 
 type instr =
 | Add  of opnd * opnd
+| Sub  of opnd * opnd
 | Mul  of opnd * opnd
+| Div  of opnd
+| Cmp  of opnd * opnd
+| Or   of opnd * opnd
+| And  of opnd * opnd
 | Mov  of opnd * opnd
 | Push of opnd
 | Pop  of opnd
+| Setne
+| Setl
+| Setle
+| Setg
+| Setge
+| Sete
+| Cltd
 | Call of string
 | Ret
-
+| Lbl     of int
+| Jz      of int
+| Jnz     of int
+| Jmp     of int
+| Test    of opnd * opnd
 let to_string buf code =      
   let instr =
     let opnd = function
@@ -26,13 +42,30 @@ let to_string buf code =
       | M s -> s  
     in
     function
-      | Add (x, y) -> Printf.sprintf "addl\t%s,%s"  (opnd x) (opnd y)
-      | Mul (x, y) -> Printf.sprintf "imull\t%s,%s" (opnd x) (opnd y)
-      | Mov (x, y) -> Printf.sprintf "movl\t%s,%s"  (opnd x) (opnd y)
-      | Push x     -> Printf.sprintf "pushl\t%s"    (opnd x)
-      | Pop  x     -> Printf.sprintf "popl\t%s"     (opnd x)
-      | Call x     -> Printf.sprintf "call\t%s"      x
-      | Ret        -> "ret" 
+      | Add (x, y)    -> Printf.sprintf "addl\t%s,%s"  (opnd x) (opnd y)
+      | Sub (x, y)    -> Printf.sprintf "subl\t%s,%s"  (opnd x) (opnd y)
+      | Mul (x, y)    -> Printf.sprintf "imull\t%s,%s" (opnd x) (opnd y)
+      | Div  x        -> Printf.sprintf "idiv\t%s"     (opnd x)
+      | Or  (x, y)    -> Printf.sprintf "\torl\t%s,\t%s"   (opnd x) (opnd y)
+      | And (x, y)    -> Printf.sprintf "\tandl\t%s,\t%s"  (opnd x) (opnd y)
+      | Setne         -> "\tsetne\t%dl"
+      | Setl         -> "\tsetl\t%dl"
+      | Setle        -> "\tsetle\t%dl"
+      | Setg         -> "\tsetg\t%dl"
+      | Setge        -> "\tsetge\t%dl"
+      | Sete         -> "\tsete\t%dl"
+      | Cltd          -> "cltd"
+      | Cmp (x, y)    -> Printf.sprintf "\tcmp\t%s,\t%s"   (opnd x) (opnd y)
+      | Mov (x, y)    -> Printf.sprintf "movl\t%s,%s"  (opnd x) (opnd y)
+      | Push x        -> Printf.sprintf "pushl\t%s"    (opnd x)
+      | Pop  x        -> Printf.sprintf "popl\t%s"     (opnd x)
+      | Call x        -> Printf.sprintf "call\t%s"     x
+      | Ret           -> "ret"
+      | Lbl x         -> Printf.sprintf "lbl%d:"       x
+      | Jz x          -> Printf.sprintf "jz\t\tlbl%d"    x
+      | Jnz x         -> Printf.sprintf "jnz\t\tlbl%d"   x
+      | Jmp x         -> Printf.sprintf "jmp\t\tlbl%d"   x
+      | Test (x, y)   -> Printf.sprintf "testl\t%s,%s" (opnd x) (opnd y)
   in
   let out s = 
     Buffer.add_string buf "\t"; 
@@ -75,23 +108,64 @@ let rec sint env prg sstack =
 	| ST x ->
             let env' = env#local x in
             let s :: sstack' = sstack in
-            env', [Mov (s, M x)], sstack' 
+        	(match s with
+       		| S _ ->  env', [Mov (s, edx); Mov (edx, M x)], sstack'
+        	| _   ->  env', [Mov (s, M x)], sstack') 
         | READ  ->
             env, [Call "lread"], [eax]
         | WRITE ->
             env, [Push eax; Call "lwrite"; Pop edx], [] 
+        | LBL x -> env, [Lbl x], []
+        | JMP x -> env, [Jmp x], []
+
+        | CJMP (x,c) -> 
+        let s :: sstack' = sstack in
+        let jmpop = (match c with "z"  -> Jz x | "nz" -> Jnz x) in
+        (match s with
+        | S _ ->  env, [Mov (s, edx); Test (edx, edx); jmpop], []
+        | _   ->  env, [Test (s, s); jmpop], [])
+
         | _ ->
             let x::(y::_ as sstack') = sstack in
-            (fun op ->
-              match x, y with
-              | S _, S _ -> env, [Mov (y, edx); op x edx; Mov (edx, y)], sstack'
-              | _        -> env, [op x y], sstack'   
+	match x, y with
+          | S _, S _ ->    
+            (match i with
+            | MUL         -> env, [Mov (y, edx);Mul(x, edx);Mov (edx, y)], sstack'
+            | DIV         -> env, [Mov (y, eax); Cltd; Div x; Mov (eax, y)], sstack'
+	    | DIVD        -> env, [Mov (y, eax); Cltd; Div x; Mov (edx, y)], sstack'
+            | ADD         -> env, [Mov (y, edx);Add(x, edx); Mov (edx, y)], sstack'
+            | SUB         -> env, [Mov (y, edx);Sub(x, edx); Mov (edx, y)], sstack'
+            | OR          -> env, [Mov (y, edx);Or(x, edx); Mov (L 0, edx); Setne;Mov (edx, y)], sstack'
+            | AND         -> env, [And (eax, eax);Mov (x, edx);Cmp (edx, eax);Setne;Mov (y, edx);Mul (eax, edx);
+                                    And (eax, eax);Cmp (edx, eax);Setne;Mov (eax, y)], sstack'
+	    
+
+            | EQUAL       -> env,[Mov (y, edx);Cmp(x, edx); Mov (L 0, edx); Sete;Mov (edx, y)], sstack'
+            | NOTEQUAL    -> env,[Mov (y, edx);Cmp(x, edx); Mov (L 0, edx); Setne;Mov (edx, y)], sstack'
+            | LESS        -> env,[Mov (y, edx);Cmp(x, edx); Mov (L 0, edx); Setl;Mov (edx, y)], sstack'
+            | GREATER     -> env,[Mov (y, edx);Cmp(x, edx); Mov (L 0, edx); Setg;Mov (edx, y)], sstack'
+            | LESSEQUAL   -> env,[Mov (y, edx);Cmp(x, edx); Mov (L 0, edx); Setle;Mov (edx, y)], sstack'
+            | GREATEREQUAL-> env,[Mov (y, edx);Cmp(x, edx); Mov (L 0, edx); Setge;Mov (edx, y)], sstack'
             )
-              (match i with 
-	      | MUL -> fun x y -> Mul (x, y)
-	      | ADD -> fun x y -> Add (x, y)
-              )
-      in
+
+          | _ ->           
+            (match i with
+            | MUL         -> env, [Mul(x, y)], sstack'
+            | DIV         -> env, [Mov (y, eax); Cltd; Div x; Mov (eax, y)], sstack'
+            | DIVD        -> env, [Mov (y, eax); Cltd; Div x; Mov (edx, y)], sstack'
+            | ADD         -> env, [Add(x, y)], sstack'
+            | SUB         -> env, [Sub(x, y)], sstack'
+            | OR          -> env, [Or(x, y); Mov (L 0, edx); Setne; Mov (edx, y)], sstack'
+            | AND         -> env, [And (eax, eax);Mov (x, edx);Cmp (edx, eax);Setne;Mov (y, edx);Mul (eax, edx);
+                                    And (eax, eax);Cmp (edx, eax);Setne;Mov (eax, y)], sstack'
+	    | EQUAL       -> env,[Cmp(x, y); Mov (L 0, edx);Sete;Mov (edx, y)], sstack'
+            | NOTEQUAL    -> env,[Cmp(x, y); Mov (L 0, edx);Setne;Mov (edx, y)], sstack'
+            | LESS        -> env,[Cmp(x, y); Mov (L 0, edx);Setl;Mov (edx, y)], sstack'
+            | GREATER     -> env,[Cmp(x, y); Mov (L 0, edx);Setg;Mov (edx, y)], sstack'
+            | LESSEQUAL   -> env,[Cmp(x, y); Mov (L 0, edx);Setle;Mov (edx, y)], sstack'
+            | GREATEREQUAL-> env,[Cmp(x, y); Mov (L 0, edx);Setge;Mov (edx, y)], sstack'
+            )
+    in
       let env, code', sstack'' = sint env prg' sstack' in
       env, code @ code', sstack''
 	
